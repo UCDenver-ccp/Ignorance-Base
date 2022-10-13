e72b5ec1045f83a60e13fba13877e5afe03ecfa7
mboguslav <mboguslav37@gmail.com>
2022-10-13 11:12:57 -0600

    combine all data ignorance and OBOs

--- Ignorance_Network/run_combine_all_data.sh
+++ Ignorance_Network/run_combine_all_data.sh
@@ -36,5 +36,34 @@ old_metadata_folder='1_First_Full_Annotation_Task_9_13_19/document_collection_pr
 meta='.meta'
 #cp $all_file_path$old_metadata_folder/*.meta $all_file_path$ignorance_base$all_data_corpus$articles_metadata/
 
+
+##combine all obos
+OBO_ontologies='CHEBI,CL,GO_BP,GO_CC,GO_MF,MOP,NCBITaxon,PR,SO,UBERON'
+OBO_EXT_ontologies='CHEBI_EXT,CL_EXT,GO_BP_EXT,GO_CC_EXT,GO_MF_EXT,MOP_EXT,NCBITaxon_EXT,PR_EXT,SO_EXT,UBERON_EXT'
+obos='OBOs/'
+concept_system_output='concept_system_output/'
+obos_bionlp_files='z_BIONLP_BEST_MODELS_OBOs/'
+obos_ext_bionlp_files='z_BIONLP_BEST_MODELS_OBOs_EXT/'
+obos_sentence_output='ALL_DATA_COMBINED_OBOs_sentences/'
+obos_dictionary_output='ALL_DATA_COMBINED_OBOs_dictionary_files/'
+obos_ext_dictionary_output='ALL_DATA_COMBINED_OBOs_EXT_dictionary_files/'
+obos_all_combined_data='0_all_combined_OBOs/'
+obos_ext_all_combined_data='0_all_combined_OBOs_EXT/'
+#/Users/MaylaB/Dropbox/Documents/0_Thesis_stuff-Larry_Sonia/Negacy_seq_2_seq_NER_model/Concept-Recognition-as-Translation/Output_Folders/Concept_Norm_Files/GO_BP/full_files
+negacy_full_files='Negacy_seq_2_seq_NER_model/Concept-Recognition-as-Translation/Output_Folders/Concept_Norm_Files/'
+
+
+
+##combine all ignorance information
 python3 combine_all_data.py -all_lcs_path=$all_file_path$ignorance_base$all_data_corpus$word_results$knowtator_projects$all_data_results$all_lcs_path -included_articles=$included_articles -ignorance_ontologies=$ignorance_ontologies -sentence_file_path=$all_file_path$ignorance_base$all_data_corpus$pmcid_sentence_files_path -bionlp_file_path=$all_file_path$ignorance_base$all_data_corpus$word_results$ignorance_bionlp_files -sentence_output_folder=$all_file_path$ignorance_base$ignorance_network$sentence_output -dictionary_output_folder=$all_file_path$ignorance_base$ignorance_network$dictionary_output -all_combined_data=$all_combined_data -it_occurence_count=$it_occurence_count -output_folder=$all_file_path$ignorance_base$ignorance_network -meta_data_article_file_path=$all_file_path$ignorance_base$all_data_corpus$articles_metadata
+
+
+##combine all OBO information both OBO and OBO_EXT
+#OBO
+python3 combine_all_data.py -all_lcs_path=$all_file_path$ignorance_base$all_data_corpus$word_results$knowtator_projects$all_data_results$all_lcs_path -included_articles=$included_articles -ignorance_ontologies=$ignorance_ontologies -sentence_file_path=$all_file_path$ignorance_base$all_data_corpus$obos$pmcid_sentence_files_path -bionlp_file_path=$all_file_path$ignorance_base$all_data_corpus$obos$obos_bionlp_files -sentence_output_folder=$all_file_path$ignorance_base$ignorance_network$obos_sentence_output -dictionary_output_folder=$all_file_path$ignorance_base$ignorance_network$obos_dictionary_output -all_combined_data=$obos_all_combined_data -it_occurence_count=$it_occurence_count -output_folder=$all_file_path$ignorance_base$ignorance_network -meta_data_article_file_path=$all_file_path$ignorance_base$all_data_corpus$articles_metadata --obo_ontologies=$OBO_ontologies --unique_obos_path=$all_file_path$all_data_corpus$obos$concept_system_output
+
+#OBO_EXT
+python3 combine_all_data.py -all_lcs_path=$all_file_path$ignorance_base$all_data_corpus$word_results$knowtator_projects$all_data_results$all_lcs_path -included_articles=$included_articles -ignorance_ontologies=$ignorance_ontologies -sentence_file_path=$all_file_path$ignorance_base$all_data_corpus$obos$pmcid_sentence_files_path -bionlp_file_path=$all_file_path$ignorance_base$all_data_corpus$obos$obos_ext_bionlp_files -sentence_output_folder=$all_file_path$ignorance_base$ignorance_network$obos_sentence_output -dictionary_output_folder=$all_file_path$ignorance_base$ignorance_network$obos_ext_dictionary_output -all_combined_data=$obos_ext_all_combined_data -it_occurence_count=$it_occurence_count -output_folder=$all_file_path$ignorance_base$ignorance_network -meta_data_article_file_path=$all_file_path$ignorance_base$all_data_corpus$articles_metadata --obo_ontologies=$OBO_EXT_ontologies --unique_obos_path=$all_file_path$all_data_corpus$obos$concept_system_output
+
+
 
